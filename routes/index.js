a6fdcf269dde6ed9a99aca3174411b494280d759
Eric Tseng <etseng@ucsd.edu>
2016-11-26 20:11:25 -0800

    bleh

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,11 @@
 var data = require('../data.json');
 
 exports.view = function(req, res){
+	data['showAlternate'] = false;
+	res.render('index', data);
+};
+
+exports.view2 = function(req, res){
+	data['showAlternate'] = true;
 	res.render('index', data);
 };
